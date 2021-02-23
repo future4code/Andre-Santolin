4a808b434710f04f99b562bbc837feaea12b9c21
AndreSantolin <andresantolin862@gmail.com>
2021-02-23 15:51:59 -0300

    operadores

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,89 @@
+//exercicio 1
+
+//const bool1 = true//
+//const bool2 = false//
+//const bool3 = !bool2//
+
+//let resultado = bool1 && bool2
+//console.log("a. ", resultado)
+//false
+
+//resultado = bool1 && bool2 && bool3
+//console.log("b. ", resultado)//
+//false
+
+//resultado = !resultado && (bool1 || bool1)
+//console.log("c. ", resultado)
+//true
+
+//console.log("e. ", typeof resultado)
+// tipo boolean
+
+//exercicio 2
+
+//let array
+//console.log('a. ', array)
+//undefined
+
+//array = null
+//console.log('b. ', array)
+//null
+
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)
+//11
+
+//let i = 0
+//console.log('d. ', array[i])
+//3
+
+//array[i+1] = 19
+//console.log('e. ', array)
+// [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+//const valor = array[i+6]
+//console.log('f. ', valor)
+//9
+
+//exercicio 3
+
+// const minhaIdade = prompt("qual a minha idade?")
+// const idadeAmigo = prompt("Qual a idade do seu melhor amigo?")
+// let resposta = (minhaIdade) > (idadeAmigo)
+// let diferença = (minhaIdade) - (idadeAmigo)
+
+// console.log("Sua idade é maior do que a do seu melhor amigo? " + resposta)
+// console.log("diferença de idade " + diferença)
+
+//exercicio 4
+
+//let numeroPar = prompt("digite um numero par")
+//let resultado = Number(numeroPar)%2
+//console.log(resultado)
+// o resultado da sobra da divisão sempre vai ser 0 quando o numero for par dividido por 2
+// inserindo um numero impar ele deixa uma sobra de divisão
+
+//const listaDeTarefas = array =[0, 1, 2]
+
+//let tarefaUm = prompt("Digite a primeira tarefa que você precisa fazer hoje")
+//let tarefaDois = prompt("Digite a segunda tarefa que você precisa fazer hoje")
+//let tarefaTres = prompt("Digite a terceira tarefa que você precisa fazer hoje")
+  
+//listaDeTarefas.push(tarefaUm) 
+//listaDeTarefas.push(tarefaDois)
+//listaDeTarefas.push(tarefaTres)
+//console.log(listaDeTarefas)
+
+//let i = prompt("Qual indice das tarefas você ja realizou? 0, 1 ou 2")
+
+//listaDeTarefas.splice(Number(i), 1)
+//console.log(listaDeTarefas)
+
+//exercicio 5
+
+//const nomeDoUsuario = prompt("Qual o seu nome?")
+//const emailDoUsuario = prompt("Qual o seu e-mail?")
+
+//const resposta = ("O e-mail " + emailDoUsuario + "foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + "!")
+// console.log(resposta)
+
