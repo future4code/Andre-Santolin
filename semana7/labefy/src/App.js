66b41d200ad1839c2f9d02baacff53cec9dc517d
AndreSantolin <andresantolin862@gmail.com>
2021-03-26 14:47:19 -0300

    inicio do projeto labefy

---
+++ semana7/labefy/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components'
+import AddPlaylist from './components/AddPlaylist'
+import SearchPlaylist from './components/SearchPlaylist'
+
+const All = styled.div `
+  text-align: center;
+  background-color: black;
+  color: white;
+  width: 100vw;
+  height: 100vh;
+`
+
